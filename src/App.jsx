1069eb344d10bb87966f99ffe2b2920da466c6b3
kraccoon-dev <kraccoon@kakao.com>
2022-04-14 21:46:26 +0900

    refactor: change to react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen	">
+      <div className="bg-white font-semibold text-center rounded-3xl border shadow-lg max-w-xs w-1/2">
+        <div className=" w-100 h-64 shadow-sm bg-indigo-900"></div>
+        <h2 className="text-lg text-left text-gray-700 pl-5 pt-5"> INDIGO </h2>
+        <h3 className="text-sm text-left text-gray-400 pl-5 pb-5"> #312E81 </h3>
+      </div>
+    </div>
+  )
+}
