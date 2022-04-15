e10248bd2badf23ec6a5ed19731e9e6a396720bc
kraccoon-dev <kraccoon@kakao.com>
2022-04-15 23:06:24 +0900

    style: base style

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import './index.css'
 
 export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen	">
-      <div className="bg-white font-semibold text-center rounded-3xl border shadow-lg max-w-xs w-1/2">
-        <div className=" w-100 h-64 shadow-sm bg-indigo-900"></div>
-        <h2 className="text-lg text-left text-gray-700 pl-5 pt-5"> INDIGO </h2>
-        <h3 className="text-sm text-left text-gray-400 pl-5 pb-5"> #312E81 </h3>
-      </div>
-    </div>
+    <>
+      <h1>Heading1</h1>
+      <h2>Heading2</h2>
+      <h3>Heading3</h3>
+      <h4>Heading4</h4>
+      <h5>Heading5</h5>
+      <h6>Heading6</h6>
+      <p className="text-sys-green">Lorem ipsum dolor sit amet.</p>
+      <p className="text-sys-red">Lorem ipsum dolor sit amet.</p>
+      <p className="text-blue">Lorem ipsum dolor sit amet.</p>
+      <p className="explainText1">Lorem ipsum dolor sit amet.</p>
+      <p className="explainText2">Lorem ipsum dolor sit amet.</p>
+      <p className="explainText">Lorem ipsum dolor sit amet.</p>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ['./src/**/*.jsx'],
   theme: {
-    extend: {},
+    colors: {
+      'gray': {
+        100: '#141517',
+        200: '#5e5e67',
+        300: '#a9a9b5',
+        400: '#e8e8e8',
+        500: '#f3f3f3 ',
+      },
+      'sys': {
+        'green': '#95c235',
+        'red': '#dd3b3b',
+      },
+      'blue': '#1372a7',
+    },
+    letterSpacing: {
+      normal: '-0.01em',
+    },
   },
+  extends: {},
   plugins: [],
 }
